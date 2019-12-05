0d7a5ecffb1cbbc2bc2266e6093159e064c0cdf9
Tom Murphy <wolfmurphy@gmail.com>
2019-12-05 15:17:04 -0800

    Even more improved day04. Now very general

--- day04a.py
+++ day04a.py
@@ -1,18 +1,19 @@
 #!/usr/local/bin/python3
 import sys
 count = 0
-def isIncreasing(lis):
-    for i in range(0,5):
+def isIncreasing(lis,start,end):
+    for i in range(start,end):
         if lis[i] > lis[i+1]:
             return False
     return True
-def hasRepeated(lis):
-    for i in range(0,5):
+def hasRepeated(lis,start,end):
+    for i in range(start,end):
         if lis[i] == lis[i+1]:
             return True
     return False
 for i in range(183564,657474):
     lis = [char for char in str(i)]
-    if isIncreasing(lis) and hasRepeated(lis):
+    start, end = 0, len(lis) - 1
+    if isIncreasing(lis,start,end) and hasRepeated(lis,start,end):
         count = count + 1
 print count
--- day04b.py
+++ day04b.py
@@ -1,18 +1,19 @@
 #!/usr/local/bin/python3
 import sys
 count = 0
-def isIncreasing(lis):
-    for i in range(1,6):
+def isIncreasing(lis,start,end):
+    for i in range(start,end):
         if lis[i] > lis[i+1]:
             return False
     return True
-def hasRepeated(L):
-    for i in range(1,6):
+def hasRepeated(L,start,end):
+    for i in range(start,end):
         if (L[i] == L[i+1]) and (L[i-1] != L[i]) and (L[i+1] != L[i+2]):
             return True
     return False
 for i in range(183564,657474):
     lis = [char for char in ('-'+str(i)+'-')]
-    if isIncreasing(lis) and hasRepeated(lis):
+    start, end = 1, len(lis) - 2
+    if isIncreasing(lis,start,end) and hasRepeated(lis,start,end):
         count = count + 1
 print count
